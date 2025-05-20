5ee161917c8d4b2037c23fdc6d807adacc719502
Gedik Mehmet-Aziz <gedikmehmetaziz@gmail.com>
2025-05-20 20:22:35 +0200

    Jour 1 : exos Python dans le bon dossier

---
+++ exos_jour1.py
@@ -0,0 +1,18 @@
+#initialisation à python
+epargne=1000
+taux=0.75
+epargne2=epargne*taux
+print(epargne2,"\n" ,"salut")
+
+txt="salut"
+txt1= txt + "wsh "
+txt2=txt1 + txt
+print(txt2)
+
+
+print(len(txt2), type(txt2))
+
+phrase= "voici une phrase"
+
+phrase=phrase.upper()
+print(phrase)
