ec322237f66eb98ee6c27707b16b622895d9700c
Gedik Mehmet-Aziz <gedikmehmetaziz@gmail.com>
2025-05-23 20:45:41 +0200

    Jour 3 , conditionet boucles

---
+++ exos_jour3.py
@@ -0,0 +1,41 @@
+nom=input("Vincent")
+print("\n")
+
+if nom=="we":
+    print(nom)
+else:
+    print("wee")
+
+print("\n")
+
+Vrai=True
+print(Vrai)
+print("\n")
+
+Faux=False
+print(Faux)
+print("\n")
+
+Et=Vrai and Faux
+Ou=Vrai or Faux
+print(Et,"\n",Ou)
+
+print("\n")
+liste=[2,"M",3]
+2 in liste
+print("\n")
+7 in liste 
+
+a=5
+b=10
+c=7
+
+if c<a :
+    print(c , "  est plus petit que ", a)
+elif c>b :
+    print(b , "  est plus petit que ", c)   
+elif c>a and c<b :
+    print(c , "  est entre  ", a, "  et ",b , "\n")
+else:
+    print("rahh weee")
+
