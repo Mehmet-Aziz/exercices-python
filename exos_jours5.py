0dd3f3e624b7b9f7d21f2ca71180b44fa55ffdee
Gedik Mehmet-Aziz <gedikmehmetaziz@gmail.com>
2025-05-24 20:15:10 +0200

    Jour 5 commencement de France IOI

---
+++ exos_jours5.py
@@ -0,0 +1,38 @@
+
+#          Niveau 1-1
+#           6 exos
+
+#        Exo 1 :affichage de texte
+
+#print("Hello world!")
+
+#          exo 2 : remettre dans l'ordre
+
+#print("Coucou !")
+#print("Je m'appelle Camthalion")
+#print("Ma devise est 'Parler peu mais parler bien'.")
+
+#       exo 3 : faire afficher un texte
+#print("Tout droit tu grimperas,","\n","La clé tu trouveras,","\n" ,"Habile tu seras,","\n","Quand tu les porteras," ,"\n" ,"Et avec le chef tu reviendras !")
+
+#       exo 4 :deplacer un robot 
+
+#       exo 5 : tour d'hanoĩ
+
+#       exo 6 : bouteille d'eau
+
+#       resolution des 6 exos , passage au niveau 1-2
+
+#       10 exos
+
+
+
+
+
+
+
+
+
+
+
+
