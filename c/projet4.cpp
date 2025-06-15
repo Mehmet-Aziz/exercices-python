5c34d251d6ae2a9e16cea6d4f1b0b148d65419d7
Gedik Mehmet-Aziz <gedikmehmetaziz@gmail.com>
2025-06-15 06:40:08 +0200

    Amelioration , toujours des fautes mais oklm

--- c/projet4.cpp
+++ c/projet4.cpp
@@ -5,147 +5,156 @@
 using namespace std;
 
 /*
-🧩 Fonctionnalités attendues :
+    🧩 Objectif général
 
-    Ajouter une tâche
+Créer une To-Do List en C++ qui permet de gérer des tâches avec :
 
-        Demander une description
+    Une description
 
-        Demander une priorité (1 à 5)
+    Une priorité (1 à 5)
 
-        Enregistrer la date de création automatiquement (facultatif : demander à l’utilisateur)
+    Une date
 
-    Afficher toutes les tâches
-
-        Lister les tâches avec leur état (à faire / terminée), description et priorité.
-
-    Marquer une tâche comme terminée
-
-        Rechercher une tâche par son indice ou titre
-
-        Modifier son état.
-
-    Supprimer une tâche
-
-        Par indice ou description.
-
-    Filtrer ou trier les tâches
-
-        Ex. : trier par priorité ou filtrer les tâches "à faire" uniquement.
-
-    Quitter le programme
+    Un état ("à faire" ou "terminée")
 */
 
 struct tache
 {
+    string nom;
     string description;
     int priorite;
     bool etat;
     char date[10];
 };
 
-void ajoute_tache(tache &t)
+void remplir (tache &t)
 {
-    cout<<"quelle est la description ?"<<endl;
+    cout<<"quelle est son nom ?"<<endl;
+    cin>>t.nom;
+    cout<<"quelle est sa description ?"<<endl;
     cin>>t.description;
-    cout<<"quelle est l'ordre de prorité classé de 1 à 5 inclus ?"<<endl;
+    cout<<"quelle est son ordre de priorite ?"<<endl;
     cin>>t.priorite;
-    cout<<"quelle est l'etat ? 0 pour à faire sinon 1 pour terminer"<<endl;
+    cout<<"quelle est son etat ?"<<endl;
     cin>>t.etat;
-    cout<<"date ? sous format JJ/MM/AAAA"<<endl;
+    cout<<"quelle est sa date limite  ?"<<endl;
     for(int i=0;i<10;i++)
     {
-        if((i==2)&&(i==5))
+        if((i==2)||(i==5))
         {
-            t.date[i]=='/';
+            t.date[i]= /;
         }
         else
         {
             cin>>t.date[i];
         }
     }
-    cout<<endl;
-
 }
 
-
-void affiche_tache(tache t)
+void affiche(vector<tache> t)
 {
-    cout<<"Voici la description :"<<endl;
-    cout<<t.description<<endl;
-    cout<<"Voici la priorite :"<<endl;
-    cout<<t.priorite<<endl;
-    cout<<"Voici l'etat :"<<endl;
-    cout<<t.etat<<endl;
-    cout<<"Voici la date crée :"<<endl;
-    for(int i=0;i<strlen(t.date);i++)
-    {
-        cout<<t.date[i];
-    }
-    cout<<endl;
+    cout<<"NOM :"<<endl<<t.nom<<endl;
+    cout<<"DESCRIPTION :"<<endl<<t.description<<endl;
+    cout<<"PRIORITE :"<<endl<<t.priorite<<endl;
+    cout<<"ETAT :"<<endl<<t.etat<<endl;
+    cout<<"DATE :"<<endl<<t.date<<endl;
 }
 
-void terminer(bool etat)
+tache terminer()
 {
-    cout<<"tu as terminer ta tache , bravo"<<endl;
-    etat=1;
+    int choix;
+    int index;
+    string titre;
+
+
+    cout<<"cherchez vous l'indice ou le nom ?"<<endl<<"pour l'indice taper 1 , pour le nom taper 2"<<;
+    cin>>choix;
+    if(choix==1)
+    {
+        cout<<"tu as chercher l'indice du tableau, tape son indice ";
+        cin>>index;
+        if(index>=0 && index<t.size())
+        {
+        t[index].etat=true;
+        }
+    }
+    bool trouve=false;
+    if(choix==2)
+    {
+        cout<<"tu as choisis le nom "<<endl<<"tape le titre ";
+        cin>>titre;
+        cin.ignore();
+        for (auto &ta : t)
+        {
+            if (ta.description == titre)
+            {
+                ta.etat = true;
+                cout << "Tâche marquée comme terminée ✅" << endl;
+                trouve = true;
+                break;
+            }
+        }
+    }
 }
 
-void recherche(tache &t)
+//fait avec chatgpt car trop debutant pour les vectors
+void supprimer_tache(vector<tache> &liste, int index)
 {
-    bool oui;
-    bool ouii;
-    bool ouiii;
-    string noom;
-    cout<<"tu cherche par rapport a son indice , tape 0 non , sinon 1 oui?"<<endl;
-    cin>>oui;
-    if(oui==1)
+    if (index < 0 || index >= liste.size())
     {
-    cout<<"tape son indice"<<endl;
-    cin>>t.priorite;
-    }
-    cout<<endl<<"tu cherche par description ? 0 pour non , 1 pour oui"<<endl;
-    cin>>ouii;
-    if(ouii==1)
-    {
-    cout<<"tape le nom"<<endl;
-    cin>>noom;
-    if(strcmp(t.description,noom)==1)
-    {
-        strcpy(t.description,noom);
-    }
-    }
-    cout<<"modifiez son etat , choisis nouveau etat ? , 0 en cours , 1 finis"<<endl;
-    cin>>ouiii;
-    if(ouiii==1)
-    {
-        cin>>t.etat;
+        cout << "❌ Index invalide. Aucun élément supprimé." << endl;
+        return;
     }
 
+    liste.erase(liste.begin() + index);
+    cout << "✅ Tâche supprimée avec succès !" << endl;
 }
 
-//j'ai pas trop compris les fonctions sont groupés comment , je marrete a la;
+filtrer_a_faire(vector<tache> &t)
+{
+    for (auto &ta :t)
+    {
+        if ta.etat==false;
+        cout<<ta.nom<<" a faire "<<endl;
+    }
+}
 
 int main()
 {
-    vector<tache> t;
-    bool fin==0;
+    cout<<"bienvenu dans le programme to do list"<<endl<<"vous pouvez "<<endl;
+    cout<<"1. Crée une tache , 2. Afficher une tache , 3. Terminer une tache , 4. Montrer ceux qui reste , 5. QUITTER"<<endl;
+    int choix;
+    vector <tache> t;
+    tache temp;
+    bool fin=false;
     do
     {
-        tache temp;
-        bool oui;
-        ajoute_tache(temp);
-        affiche_tache(temp);
-        cout<<"tache terminer ? 0 pour non , 1 pour oui"<<endl;
-        cin>>oui;
-        if(oui==1)
+        cout<<"Veuillez choisir"<<endl;
+        cin>>choix;
+        switch(choix)
         {
-            terminer(temp.etat);
+            case 1 :
+            remplir(temp);
+            t.push_back(temp);
+            break;
+
+             case 2 :
+            affiche(temp);
+            break;
+
+             case 3 :
+            terminer(temp);
+            break;
+
+            case 4 :
+            filtrer_a_faire(t);
+            break;
+
+            case 5 :
+            fin=true;
+            break;
         }
-        t.push_back(temp);
-        cout<<endl<<"tu as finis ? 0 pour non , 1 pour oui"<<end;
-        cin>>fin;
-    } while (fin==0);
-    recherche(t);
+    } while (fin==false);
+
     return 0;
 }
