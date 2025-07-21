5045e846fde2471697c672f02ee5431d84e10b77
Mehmet-Aziz <gedikmehmetaziz@gmail.com>
2025-07-21 21:48:16 +0200

    fin pong

--- c/projet41.cpp
+++ c/projet41.cpp
@@ -36,6 +36,14 @@ int main()
     RenderWindow window(VideoMode(1000,1000),"pong");
 
 
+        Font font;
+        if(!font.loadFromFile("DejaVuSans.ttf"))
+        {
+            cerr<<"erreur"<<endl;
+            return 1;
+        }
+
+
         float rect1X=0 , rect1Y=500;
         float rect1L=10 , rect1H=100;
 
@@ -54,23 +62,45 @@ int main()
         rond.setFillColor(Color::Blue);
 
 
+        float rectQuitterX=500 , rectQuitterY=500;
+        float rectQuitterL=100 , rectQuitterH=100;
+        RectangleShape rectQuitter(Vector2f(rectQuitterL,rectQuitterH));
+        rectQuitter.setFillColor(Color::White);
+        Text txtQuitter("QUITTER" , font ,20);
+        txtQuitter.setFillColor(Color::Black);
+
+
+
+        float rectJouerX=500 , rectJouerY=300;
+        float rectJouerL=100 , rectJouerH=100;
+        RectangleShape rectJouer(Vector2f(rectJouerL,rectJouerH));
+        rectJouer.setFillColor(Color::White);
+        Text txtJouer("JOUER" , font , 20);
+        txtJouer.setFillColor(Color::Black);
+
+        int score1 =0 , score2=0;
+
+        Text txtscore1;
+        txtscore1.setFont(font);
+        txtscore1.setFillColor(Color::Red);
+        txtscore1.setCharacterSize(15);
+
+        Text txtscore2;
+        txtscore2.setFont(font);
+        txtscore2.setFillColor(Color::Red);
+        txtscore2.setCharacterSize(15);
+
+
+
+
+
+
 
     while(window.isOpen())
     {
         window.clear();
         Event event;
-
-
-        b.x+=b.vx;
-        b.y+=b.vy;
-        float d1=sqrt(pow(b.x-rect1X,2)+pow(b.y-rect1Y,2));
-        float d2=sqrt(pow(b.x-rect2X,2)+pow(b.y-rect2Y,2));
-        if(b.y<0 || b.y>1000 || d1<b.r || d2<b.r)
-        {
-            b.vy=-b.vy;
-        }
         
-
 
         while(window.pollEvent(event))
         {
@@ -79,22 +109,54 @@ int main()
                 window.close();
             }
 
-        if (Keyboard::isKeyPressed(Keyboard::Up))
+
+
+            if(etat==false && event.type==Event::MouseButtonPressed && event.mouseButton.button==Mouse::Left)
+            {
+                float sourisX=event.mouseButton.x , sourisY=event.mouseButton.y;
+                if(sourisX>=rectQuitterX-rectQuitterL/2 && sourisX<=rectQuitterX+rectQuitterL/2 && sourisY>=rectQuitterY-rectQuitterH/2 && sourisY<=rectQuitterY+rectQuitterH/2)
+                {
+                    window.close();
+                }
+                if(sourisX>=rectJouerX-rectJouerL/2 && sourisX<=rectJouerX+rectJouerL/2 && sourisY>=rectJouerY-rectJouerH/2 && sourisY<=rectJouerY+rectJouerH/2)
+                {
+                    etat=true;
+                }
+
+            }
+
+
+            
+
+
+        if (etat==true && Keyboard::isKeyPressed(Keyboard::Up))
         {
             rect1Y-=10;
         }
-        if (Keyboard::isKeyPressed(Keyboard::Down))
+        if (etat==true && Keyboard::isKeyPressed(Keyboard::Down))
         {
             rect1Y+=10;
         }
-        if (Keyboard::isKeyPressed(Keyboard::Z))
+        if (etat==true && Keyboard::isKeyPressed(Keyboard::Z))
         {
             rect2Y-=10;
         }
-        if (Keyboard::isKeyPressed(Keyboard::S))
+        if (etat==true && Keyboard::isKeyPressed(Keyboard::S))
         {
             rect2Y+=10;
         }
+        if(etat==true && b.x<0)
+        {
+            score2+=1;
+            b=ajoute_balle();
+        }
+
+        if(etat==true && b.x>1000)
+        {
+            score1+=1;
+            b=ajoute_balle();
+        }
+
 
 
 
@@ -103,21 +165,79 @@ int main()
         }
 
 
+        if(etat==false)
+        {
+
+
+            txtJouer.setPosition(rectJouerX-30,rectJouerY-10);
+            txtQuitter.setPosition(rectQuitterX-40,rectQuitterY-10);
+            rectQuitter.setPosition(rectQuitterX-rectQuitterL/2,rectQuitterY-rectQuitterH/2);
+            rectJouer.setPosition(rectJouerX-rectJouerL/2,rectJouerY-rectJouerH/2);
+            window.draw(rectQuitter);
+            window.draw(txtQuitter);
+            window.draw(rectJouer);
+            window.draw(txtJouer);
+
+        }
+        
+
+
+        if(etat==true)
+        {
+
+        b.x+=b.vx;
+        b.y+=b.vy;
+
+        if( b.x < rect1X + rect1L &&  b.x + 2*b.r > rect1X &&  b.y + 2*b.r > rect1Y && b.y < rect1Y + rect1H  )
+        {
+            b.vx=-b.vx*1.1076;
+            b.vy=-b.vy*1.1076;
+        }
+        if( b.x < rect2X + rect2L &&  b.x + 2*b.r > rect2X &&  b.y + 2*b.r > rect2Y && b.y < rect2Y + rect2H  )
+        {
+            b.vx=-b.vx*1.1076; 
+            b.vy=-b.vy*1.1076;
+        }
+
+
+        if(b.y-b.r<0 || b.y-b.r>1000)
+        {
+            b.vy=-b.vy;
+        }
+
+
+        if(rect1Y < 0)
+        {
+            rect1Y=0;
+        }
+        if(rect1Y >900)
+        {
+            rect1Y=900;
+        }
+        if(rect2Y < 0)
+        {
+            rect2Y=0;
+        }
+        if(rect2Y >900)
+        {
+            rect2Y=900;
+        }
 
 
 
-
-
-
-
-
-
+        txtscore1.setString("score : " + to_string(int(score1)));
+        txtscore2.setString("score : " + to_string(int(score2)));
+        txtscore1.setPosition(10 , 900);
+        txtscore2.setPosition(900 , 900);
         rect1.setPosition(rect1X,rect1Y);
         rect2.setPosition(rect2X,rect2Y);
         rond.setPosition(b.x,b.y);
+        window.draw(txtscore1);
+        window.draw(txtscore2);
         window.draw(rect1);
         window.draw(rect2);
         window.draw(rond);
+        }
 
 
         window.display();
