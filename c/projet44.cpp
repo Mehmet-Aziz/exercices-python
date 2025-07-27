fd48f714bf7d742ff60aaebc7a9ba8ff63f25065
Mehmet-Aziz <gedikmehmetaziz@gmail.com>
2025-07-27 13:46:43 +0200

    exos 1 pointeurs

--- c/projet44.cpp
+++ c/projet44.cpp
@@ -4,6 +4,8 @@
 using namespace std;
 using namespace sf;
 
+
+/*
 int main() {
     int age = 20;
     int *ptr = &age; // ptr pointe vers age
@@ -16,4 +18,43 @@ int main() {
     *ptr = 42; // change age via le pointeur
     cout << "Nouvelle valeur de age: " << age << endl;
     return 0;
+}
+
+*/
+
+void incremete(int *p)
+{
+    (*p)+=1;
+}
+
+void swap(int *a , int *b)
+{
+    int tmp=*a;
+    *a=*b;
+    *b=tmp;
+}
+int main()
+{
+ int a = 10;
+ int *ptr= &a;
+ cout<<ptr<<endl<<*ptr;
+
+ *ptr=20;
+ cout<<endl<<a<<endl;
+
+ int x = 10;
+ int y=20;
+ int *ptrx=&x;
+ int *ptry=&y;
+ cout<<endl<<endl<<*ptrx+*ptry<<endl;
+
+ incremete(&x);
+ cout<<endl<<*ptrx;
+
+swap(&x , &y);
+
+cout<<endl<<endl<<*ptrx<<*ptry<<endl;
+
+
+ return 0;
 }
