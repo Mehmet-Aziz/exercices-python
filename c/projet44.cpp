564191427d0bae93e6e28fd0d515c16aba85fe9b
Mehmet-Aziz <gedikmehmetaziz@gmail.com>
2025-07-27 13:23:56 +0200

    debut des pointeurs

--- c/projet44.cpp
+++ c/projet44.cpp
@@ -4,31 +4,16 @@
 using namespace std;
 using namespace sf;
 
-int main()
-{
+int main() {
     int age = 20;
-    cout<<"age"<<endl;
-    age=22;
-    int a=15 , b=7;
-    cout<<a+b<<endl<<a*b<<endl<<a/b<<endl;
+    int *ptr = &age; // ptr pointe vers age
 
-    float note=15.5;
-    double temperature=-4.2;
-    double somme = note + temperature;
-    cout<<endl<<somme<<endl;
-    note=2*note;
-    cout<<note;
+    cout << "Valeur de age: " << age << endl;
+    cout << "Adresse de age: " << &age << endl;
+    cout << "Valeur de ptr (adresse): " << ptr << endl;
+    cout << "Valeur pointee par ptr: " << *ptr << endl;
 
-    char lettre = 'A';
-    cout<<endl<<lettre<<endl;
-    lettre='Z';
-    cout<<lettre<<endl;
-
-    bool estMajeur=true;
-    cout<<endl<<estMajeur<<endl;
-    estMajeur=false;
-    cout<<estMajeur<<endl;
-
+    *ptr = 42; // change age via le pointeur
+    cout << "Nouvelle valeur de age: " << age << endl;
     return 0;
-
 }
